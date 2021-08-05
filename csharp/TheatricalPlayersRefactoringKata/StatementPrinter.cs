f7de670d9f617be0130070cd7436329e5efdc3e8
Шаймухаметов Ильдар <shaimildar@gmail.com>
2021-08-05 07:04:27 +0500

    extract function GetAmount

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -13,28 +13,10 @@ namespace TheatricalPlayersRefactoringKata
             var result = string.Format("Statement for {0}\n", invoice.Customer);
             CultureInfo cultureInfo = new CultureInfo("en-US");
 
-            foreach(var perf in invoice.Performances) 
+            foreach(var perf in invoice.Performances)
             {
                 var play = plays[perf.PlayID];
-                var thisAmount = 0;
-                switch (play.Type) 
-                {
-                    case "tragedy":
-                        thisAmount = 40000;
-                        if (perf.Audience > 30) {
-                            thisAmount += 1000 * (perf.Audience - 30);
-                        }
-                        break;
-                    case "comedy":
-                        thisAmount = 30000;
-                        if (perf.Audience > 20) {
-                            thisAmount += 10000 + 500 * (perf.Audience - 20);
-                        }
-                        thisAmount += 300 * perf.Audience;
-                        break;
-                    default:
-                        throw new Exception("unknown type: " + play.Type);
-                }
+                var thisAmount = GetAmount(perf, play);
                 // add volume credits
                 volumeCredits += Math.Max(perf.Audience - 30, 0);
                 // add extra credit for every ten comedy attendees
@@ -46,6 +28,33 @@ namespace TheatricalPlayersRefactoringKata
             }
             result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
             result += String.Format("You earned {0} credits\n", volumeCredits);
+            return result;
+        }
+
+        private static int GetAmount(Performance performance, Play play)
+        {
+            var result = 0;
+            switch (play.Type)
+            {
+                case "tragedy":
+                    result = 40000;
+                    if (performance.Audience > 30)
+                    {
+                        result += 1000 * (performance.Audience - 30);
+                    }
+                    break;
+                case "comedy":
+                    result = 30000;
+                    if (performance.Audience > 20)
+                    {
+                        result += 10000 + 500 * (performance.Audience - 20);
+                    }
+                    result += 300 * performance.Audience;
+                    break;
+                default:
+                    throw new Exception("unknown type: " + play.Type);
+            }
+
             return result;
         }
     }
