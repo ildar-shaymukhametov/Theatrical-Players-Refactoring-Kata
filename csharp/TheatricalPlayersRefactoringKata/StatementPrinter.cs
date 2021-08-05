09aae7e11f700abe655ba0c7f2eda178c8e60f9a
Шаймухаметов Ильдар <shaimildar@gmail.com>
2021-08-05 07:56:17 +0500

    enrich performance with play

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 
 namespace TheatricalPlayersRefactoringKata
 {
     public class StatementData
     {
         public string Customer { get; internal set; }
-        public List<Performance> Performances { get; internal set; }
+        public List<EnrichedPerformance> Performances { get; internal set; }
+    }
+    
+    public class EnrichedPerformance : Performance
+    {
+        public EnrichedPerformance(string playID, int audience, Play play) : base(playID, audience)
+        {
+            Play = play;
+        }
+
+        public Play Play { get; }
     }
 
     public class StatementPrinter
@@ -16,42 +27,47 @@ namespace TheatricalPlayersRefactoringKata
         {
             var data = new StatementData();
             data.Customer = invoice.Customer;
-            data.Performances = invoice.Performances;
-            return GetPlainText(data, plays);
+            data.Performances = invoice.Performances.Select(x => EnrichPerfomance(x, plays)).ToList();
+            return GetPlainText(data);
         }
 
-        private static string GetPlainText(StatementData data, Dictionary<string, Play> plays)
+        private EnrichedPerformance EnrichPerfomance(Performance performance, Dictionary<string, Play> plays)
+        {
+            return new EnrichedPerformance(performance.PlayID, performance.Audience, GetPlay(plays, performance));
+        }
+
+        private static string GetPlainText(StatementData data)
         {
             var result = string.Format("Statement for {0}\n", data.Customer);
             CultureInfo cultureInfo = new CultureInfo("en-US");
 
             foreach (var perf in data.Performances)
             {
-                result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", GetPlay(plays, perf).Name, ToUsd(GetAmount(perf, plays)), perf.Audience);
+                result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", perf.Play.Name, ToUsd(GetAmount(perf)), perf.Audience);
             }
 
-            result += String.Format(cultureInfo, "Amount owed is {0:C}\n", ToUsd(GetTotalAmount(data, plays)));
-            result += String.Format("You earned {0} credits\n", GetTotalVolumeCredit(data, plays));
+            result += String.Format(cultureInfo, "Amount owed is {0:C}\n", ToUsd(GetTotalAmount(data)));
+            result += String.Format("You earned {0} credits\n", GetTotalVolumeCredit(data));
             return result;
         }
 
-        private static int GetTotalAmount(StatementData data, Dictionary<string, Play> plays)
+        private static int GetTotalAmount(StatementData data)
         {
             var result = 0;
             foreach (var perf in data.Performances)
             {
-                result += GetAmount(perf, plays);
+                result += GetAmount(perf);
             }
 
             return result;
         }
 
-        private static int GetTotalVolumeCredit(StatementData data, Dictionary<string, Play> plays)
+        private static int GetTotalVolumeCredit(StatementData data)
         {
             var result = 0;
             foreach (var perf in data.Performances)
             {
-                result += GetVolumeCredit(perf, plays);
+                result += GetVolumeCredit(perf);
             }
 
             return result;
@@ -62,10 +78,10 @@ namespace TheatricalPlayersRefactoringKata
             return Convert.ToDecimal(amount / 100);
         }
 
-        private static int GetVolumeCredit(Performance performance, Dictionary<string, Play> plays)
+        private static int GetVolumeCredit(EnrichedPerformance performance)
         {
             var result = Math.Max(performance.Audience - 30, 0);
-            if ("comedy" == GetPlay(plays, performance).Type)
+            if ("comedy" == performance.Play.Type)
             {
                 result += (int)Math.Floor((decimal)performance.Audience / 5);
             }
@@ -77,10 +93,10 @@ namespace TheatricalPlayersRefactoringKata
             return plays[performance.PlayID];
         }
 
-        private static int GetAmount(Performance performance, Dictionary<string, Play> plays)
+        private static int GetAmount(EnrichedPerformance performance)
         {
             var result = 0;
-            switch (GetPlay(plays, performance).Type)
+            switch (performance.Play.Type)
             {
                 case "tragedy":
                     result = 40000;
@@ -98,7 +114,7 @@ namespace TheatricalPlayersRefactoringKata
                     result += 300 * performance.Audience;
                     break;
                 default:
-                    throw new Exception("unknown type: " + GetPlay(plays, performance).Type);
+                    throw new Exception("unknown type: " + performance.Play.Type);
             }
 
             return result;
