a14adf26eb88d173d24defc0af8d8d9c1b883a7a
Шаймухаметов Ильдар <shaimildar@gmail.com>
2021-08-05 08:00:13 +0500

    move method "GetAmount"

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -19,6 +19,33 @@ namespace TheatricalPlayersRefactoringKata
         }
 
         public Play Play { get; }
+
+        public int GetAmount()
+        {
+            var result = 0;
+            switch (Play.Type)
+            {
+                case "tragedy":
+                    result = 40000;
+                    if (Audience > 30)
+                    {
+                        result += 1000 * (Audience - 30);
+                    }
+                    break;
+                case "comedy":
+                    result = 30000;
+                    if (Audience > 20)
+                    {
+                        result += 10000 + 500 * (Audience - 20);
+                    }
+                    result += 300 * Audience;
+                    break;
+                default:
+                    throw new Exception("unknown type: " + Play.Type);
+            }
+
+            return result;
+        }
     }
 
     public class StatementPrinter
@@ -43,7 +70,7 @@ namespace TheatricalPlayersRefactoringKata
 
             foreach (var perf in data.Performances)
             {
-                result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", perf.Play.Name, ToUsd(GetAmount(perf)), perf.Audience);
+                result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", perf.Play.Name, ToUsd(perf.GetAmount()), perf.Audience);
             }
 
             result += String.Format(cultureInfo, "Amount owed is {0:C}\n", ToUsd(GetTotalAmount(data)));
@@ -56,7 +83,7 @@ namespace TheatricalPlayersRefactoringKata
             var result = 0;
             foreach (var perf in data.Performances)
             {
-                result += GetAmount(perf);
+                result += perf.GetAmount();
             }
 
             return result;
@@ -91,33 +118,6 @@ namespace TheatricalPlayersRefactoringKata
         private static Play GetPlay(Dictionary<string, Play> plays, Performance performance)
         {
             return plays[performance.PlayID];
-        }
-
-        private static int GetAmount(EnrichedPerformance performance)
-        {
-            var result = 0;
-            switch (performance.Play.Type)
-            {
-                case "tragedy":
-                    result = 40000;
-                    if (performance.Audience > 30)
-                    {
-                        result += 1000 * (performance.Audience - 30);
-                    }
-                    break;
-                case "comedy":
-                    result = 30000;
-                    if (performance.Audience > 20)
-                    {
-                        result += 10000 + 500 * (performance.Audience - 20);
-                    }
-                    result += 300 * performance.Audience;
-                    break;
-                default:
-                    throw new Exception("unknown type: " + performance.Play.Type);
-            }
-
-            return result;
         }
     }
 }
